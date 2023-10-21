d4ec187a23cb5a27fa8366b4f39bc614544d05e1
Zach <pittmanzach2@gmail.com>
2023-10-21 17:50:34 -0400

    Finalized part 6

--- my_game_folder/game.js
+++ my_game_folder/game.js
@@ -4,8 +4,12 @@ var config = {
   backgroundColor: 0x000000,
   scene: [Scene1, Scene2],
   pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+    },
+  },
 };
 
-window.onload = function () {
-  var game = new Phaser.Game(config);
-};
+var game = new Phaser.Game(config);
